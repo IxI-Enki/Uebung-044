d584626d6f59b6fe7e2f180e6f992d5a5206d042
IxI-Enki <cell.enohp.apps@gmail.com>
2023-12-16 21:38:55 +0100

    init

---
+++ FilterVokals/FilterVokals/FilterVocals.cs
@@ -0,0 +1,45 @@
+﻿/*-----------------------------------------------------------------------------
+ *                      HTBLA-Leonding / Class: 3ACIF                          
+ *-----------------------------------------------------------------------------
+ *                      Jan Ritt                                               
+ *-----------------------------------------------------------------------------
+ *  Description:  The Program outputs the first occurrence of a Vocal in a taxt
+ *                upper and lower case ignored
+ *-----------------------------------------------------------------------------
+*/
+
+/*_________________________________libraries_________________________________*/
+using System;                   //  
+using System.Text;              //  Unicode Symbols
+using System.Threading;         //  Thread.Sleep(1000) = 1 sec
+
+/*---------------------------------- START ----------------------------------*/
+namespace FilterVocals      //  
+{                           //
+  public class Program      //
+  {                         //
+    static void Main()      //
+    {
+      ///*----------------------- console_settings ------------------------*///
+      const int cWidth = 53;                     //  console width
+      const int cHeight = 30;                    //  & height
+      Console.SetWindowSize(cWidth, cHeight);    //
+      Console.OutputEncoding = Encoding.UTF8;    //  Unicode Symbols
+
+      /*----------------------------- VARIABLES -----------------------------*/
+      string userInput;          //  
+
+      /*-------------------------------- HEAD -------------------------------*/
+      Console.Clear();
+      Console.Write("\n             Vokale aus Text extrahieren             " +
+      /* cWidth: */ "\n=====================================================");
+
+
+
+      /*-------------------------------- END --------------------------------*/
+      Console.Write("\n Zum beenden Eingabetaste drücken..");
+      Console.ReadLine();    //  wait for [enter]
+      Console.Clear();       //
+    }
+  }
+}
