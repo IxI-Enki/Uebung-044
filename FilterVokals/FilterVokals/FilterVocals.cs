edc73738b704f74e545312a5f662fe63a81d06ea
IxI-Enki <cell.enohp.apps@gmail.com>
2023-12-16 22:32:46 +0100

    c# code

--- FilterVokals/FilterVokals/FilterVocals.cs
+++ FilterVokals/FilterVokals/FilterVocals.cs
@@ -27,14 +27,53 @@ namespace FilterVocals      //
       Console.OutputEncoding = Encoding.UTF8;    //  Unicode Symbols
 
       /*----------------------------- VARIABLES -----------------------------*/
-      string userInput;          //  
+      string userInput,                       //  
+             vocalCache = "",
+             vocalCacheOut = "";
+      char[] vocalLower = new char[5];
+      vocalLower = ['a', 'e', 'i', 'o', 'u'];
+      char[] vocalUpper = new char[5];
+      vocalUpper = ['A', 'E', 'I', 'O', 'U'];
+
+      int index;
+      bool empty;
 
       /*-------------------------------- HEAD -------------------------------*/
       Console.Clear();
       Console.Write("\n             Vokale aus Text extrahieren             " +
       /* cWidth: */ "\n=====================================================");
 
+      Console.Write("\n Text: ");
+      userInput = Console.ReadLine();
 
+      empty = (userInput.Length > 0) ? true : false;
+      if (empty)
+      {
+        for (index = 0; index < userInput.Length; index++)
+        {
+          for (int i = 0; i < 5; i++)
+          {
+            if (userInput[index] == vocalLower[i] || userInput[index] == vocalUpper[i])
+            {
+              vocalCache = vocalCache + userInput[index];
+              vocalLower[i] = ' ';
+              vocalUpper[i] = ' ';
+            }
+          }
+        }
+        int length = vocalCache.Length;
+        for (index = 0; length > index; index++)
+        {
+          if (vocalCache[index] != ' ')
+          {
+            vocalCacheOut = vocalCacheOut + vocalCache[index];
+          }
+        }
+      }
+      else
+      { Console.Write("\n"); }
+
+      Console.Write($"\n Der Text \"{userInput}\" enthält {vocalCacheOut.Length} Vokale: {vocalCacheOut}");
 
       /*-------------------------------- END --------------------------------*/
       Console.Write("\n Zum beenden Eingabetaste drücken..");
